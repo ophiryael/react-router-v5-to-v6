9bc64637b435166802272bed0937a902ba919a98
Ophir Yael <ofir.yael@gmail.com>
2022-09-09 00:05:49 +0300

    Add highlighted quote

--- phase-2/src/pages/QuoteDetail.js
+++ phase-2/src/pages/QuoteDetail.js
@@ -1,12 +1,25 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is fun!" },
+];
+
 const QuoteDetail = () => {
   const { quoteId } = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/:quoteId/comments`}>
         <Comments />
       </Route>
